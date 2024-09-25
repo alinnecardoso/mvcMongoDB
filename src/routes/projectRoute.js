9753f8d434b6ad1b9b84522f9e52a9596bf1c549
alinnecardoso <dev14cardoso@gmail.com>
2024-09-25 12:00:55 -0300

    Project Route and User Route Done

---
+++ src/routes/projectRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const projectController = require('../controllers/projectController');
+
+router.get('/project', projectController.getProject);
+router.post('/project', projectController.create);
+router.get('/project/:id', projectController.details);
+router.put('/project/:id', projectController.update);
+router.delete('/project/:id', projectController.delete)
+
+module.exports = router;
